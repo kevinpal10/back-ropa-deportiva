425f0cabc84fb6f1945af7596db2917fe79da09f
Kevin Pallo <kevin-akd1@hotmail.com>
2025-10-02 23:06:11 -0500

    configuracion final s3

--- tienda_deportiva/settings.py
+++ tienda_deportiva/settings.py
@@ -151,8 +151,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 ## Configuración de Amazon S3 para almacenamiento de archivos estáticos y multimedia
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = "tienda-deportiva-trendy-style"
-AWS_S3_REGION_NAME = "us-east-2"  # cambia según tu región
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME') # cambia según tu región
 AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
