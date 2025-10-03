131a59c6dba34d3c7ecabf41f42fc9956691fe05
Kevin Pallo <kevin-akd1@hotmail.com>
2025-10-03 00:06:09 -0500

    modificación en settings

--- tienda_deportiva/settings.py
+++ tienda_deportiva/settings.py
@@ -133,9 +133,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
@@ -156,4 +157,4 @@ AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME') # cambia según tu reg
 AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
+MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
