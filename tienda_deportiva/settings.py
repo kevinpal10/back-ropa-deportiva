7853ece865eabcff0f3276f17d2a764bfe15d42f
Kevin Pallo <kevin-akd1@hotmail.com>
2026-04-17 13:05:11 -0500

    Ahora si ultimo cambio deberitas

--- tienda_deportiva/settings.py
+++ tienda_deportiva/settings.py
@@ -65,17 +65,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'tienda_deportiva.urls'
 
-CORS_ALLOW_ALL_ORIGINS = True
-
-CORS_ALLOW_CREDENTIALS = True
-
-CORS_ALLOW_HEADERS = [
-    "content-type",
-    "authorization",
-    "x-csrftoken",
-    "accept",
-    "origin",
-]
+CORS_ALLOW_ALL_ORIGINS = False
 
 CORS_ALLOWED_ORIGINS = [
     "https://kevinpal10.github.io"
@@ -174,18 +164,8 @@ AWS_S3_FILE_OVERWRITE = False
 
 AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 
-AWS_LOCATION = 'media'
-
-STORAGES = {
-    "default": {
-        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
-    },
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
-
 
 AWS_QUERYSTRING_AUTH = False
