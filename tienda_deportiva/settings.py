b542cdc4894f9bf1d5faa1f8d648fdccbd683281
Kevin Pallo <kevin-akd1@hotmail.com>
2026-04-17 12:56:26 -0500

    Ahora si ultimo cambio deberitas

--- tienda_deportiva/settings.py
+++ tienda_deportiva/settings.py
@@ -169,7 +169,10 @@ AWS_LOCATION = 'media'
 STORAGES = {
     "default": {
         "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
-    }
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
 }
 
 MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
