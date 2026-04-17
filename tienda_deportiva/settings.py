0880d788a56c484346ea12b0986c7a48013cefab
Kevin Pallo <kevin-akd1@hotmail.com>
2026-04-17 12:29:32 -0500

    Ahora si ultimo cambio deberitas

--- tienda_deportiva/settings.py
+++ tienda_deportiva/settings.py
@@ -160,6 +160,8 @@ AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME') # cambia según tu reg
 
 AWS_DEFAULT_ACL = None
 
+AWS_S3_FILE_OVERWRITE = False
+
 AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
