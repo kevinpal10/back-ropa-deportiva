29856716dfceb51a0c64a4fe66ecccf63a40e0f1
Kevin Pallo <kevin-akd1@hotmail.com>
2026-04-17 15:09:55 -0500

    Ahora si ultimo cambio deberitas deberitas

--- tienda_deportiva/settings.py
+++ tienda_deportiva/settings.py
@@ -180,12 +180,18 @@ AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME') # cambia según tu reg
 AWS_DEFAULT_ACL = None
 
 
-
 AWS_S3_FILE_OVERWRITE = False
 
 AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
 
