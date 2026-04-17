186ab08c6718f11564371db667829ec689e08975
Kevin Pallo <kevin-akd1@hotmail.com>
2026-04-17 13:14:52 -0500

    Ahora si ultimo cambio deberitas

--- tienda_deportiva/settings.py
+++ tienda_deportiva/settings.py
@@ -65,7 +65,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'tienda_deportiva.urls'
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
 
 CORS_ALLOWED_ORIGINS = [
     "https://kevinpal10.github.io"
